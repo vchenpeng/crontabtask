460c630c5a8d52500366dfdd31ae3ebd770e0aea
peng.chen <peng.chen@freemud.com>
2022-09-14 23:47:09 +0800

    test

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 const axios = require('axios');
 
-axios.get('https://api.day.app/zuajomKPMyvheFWyMYv6yZ/test').then(res => {
+axios.post('https://api.day.app/zuajomKPMyvheFWyMYv6yZ', {
+  title: '你好',
+  body: [
+    `区域：sh`,
+    `手机：haha`
+  ].filter(x => !!x).join('\n'),
+  sound: `paymentsuccess`
+}).then(res => {
   console.log('res', res)
-})
+});
 console.log('end')
