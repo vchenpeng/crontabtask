d8283793a604cec66c2ef02cfd4e0e78b479f3c5
peng.chen <peng.chen@freemud.com>
2022-09-14 23:37:28 +0800

    test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-axios.get(`https://api.day.app/zuajomKPMyvheFWyMYv6yZ/这里改成你自己的推送内容`).then(res => {
+axios.get('https://api.day.app/zuajomKPMyvheFWyMYv6yZ/这里改成你自己的推送内容').then(res => {
   console.log('响应', res)
 })
 console.log('end')
