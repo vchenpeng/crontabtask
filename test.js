fb0b75f14e6280749a938464267956411da89e65
peng.chen <peng.chen@freemud.com>
2022-09-15 00:20:45 +0800

    test

--- test.js
+++ test.js
@@ -7,6 +7,6 @@ cron "0/35 * * * * *" script-path= test.js,tag= 临时测试
 const axios = require('axios');
 
 axios.get(`https://www.17917.cn`);
-axios.get(`https://www.buyid8.com`);
+axios.get(`https://www.wyids.com/pay/p/test`);
 
 console.log('end')
