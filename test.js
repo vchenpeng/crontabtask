eec927836a56df00933f1506d8dd7acc01980a53
peng.chen <peng.chen@freemud.com>
2023-09-05 11:38:59 +0800

    fix

--- test.js
+++ test.js
@@ -80,10 +80,25 @@ function queryWxDeveloperIssuses (cookie) {
   });
 }
 
+function reportWxDeveloper (cookie) {
+  const body = `url=https%253A%252F%252Fdevelopers.weixin.qq.com%252Fcommunity%252Fdevelop%252Farticle%253Fid%253D&useragent=Mozilla%252F5.0%2520%28Macintosh%253B%2520Intel%2520Mac%2520OS%2520X%252010_15_7%29%2520AppleWebKit%252F537.36%2520%28KHTML%252C%2520like%2520Gecko%29%2520Chrome%252F115.0.0.0%2520Safari%252F537.36&referrer=https%253A%252F%252Fdevelopers.weixin.qq.com%252Fcommunity%252Fdevelop%252Fquestion%253Fid%253D&mmdata=%5B%5B17313%2C0%5D%5D&monitor=%5B%5B%22112678%22%2C59%5D%2C%5B%22119204%22%2C0%5D%5D`;
+  axios.post(`https://developers.weixin.qq.com/community/ngi/batchreport?nosw=1`, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      cookie: cookie
+    },
+    body: body
+  }).then(res => {
+    console.log('上报微信开发者社区日志', res.data);
+    return res.data;
+  });
+}
+
 async function main () {
   console.log('测试开始')
   // await queryIp()
   const wxCookie = `bid=95697369-2de7-453d-828b-e562b9950831; bidjs=c0d4b0c6-838a-4924-8211-17803e88aa70; wxuin=89232480206062; pgv_pvid=7611979038; pac_uid=0_63f80d47c9542; iip=0; _clck=3868746640|1|fee|0; pgv_info=ssid=s9005177808; verifysession=h015b9374edea312ca4bc75cb8188c947f7a9478b43e80c274037482db6aa2b396df3bcc7f835c3a83e; RK=0YWB+yQNYj; ptcz=b8e2cfe58d6b600452398c03f4a1b738f7584a4d62f1ebbc3a37e607612f4928; data_ticket=gScI5DbUXDRy984sH3HayIm2AAAAAAAAAAAAAAAAAAA=; sid=eFl4WGYzSURSZHBxODlqOTJ1YzBJNHdBaWdha09jX1lKcEhHYU5FMVpsMjM1RU5SVU1NZmZ1N3pfTnV4aUVkbzhEaElUYm1Bb05yTzZIV1ZscVJVMTBRV3ltd0xWRjVOR2VNWlhUV1FkamFEb2ExR3J3VWVCajBPc3JQNVlwbDh3dklqQ25mN05OUVN2SUQ0; user=wxid_n2u8tw0faupj12; openid=oCJUsw0yZGqGfygntxuAaxp8Mw4U; server_token=750950383; enterprise_openid=; forum_login_type=; forum_original_openid=; qq_domain_video_guid_verify=4c59295ae0d65139; tvfe_boss_uuid=7817b777df0dd0b0`;
+  reportWxDeveloper();
   queryWxDeveloper(wxCookie);
   queryWxDeveloperIssuses(wxCookie);
   axios.get(`https://www.17917.cn`);
