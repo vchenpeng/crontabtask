f582f9a3255b428a4974e8b2a8fe1af21f26beab
peng.chen <peng.chen@freemud.com>
2023-12-09 23:31:02 +0800

    fix

--- ethi.js
+++ ethi.js
@@ -1,16 +1,15 @@
 /*
-自己测试一下
+ETHI
 
-cron "0/35 * * * * *" script-path= ethi.js,tag= 临时测试
+cron "0/35 * * * * *" script-path= ethi.js,tag= ETHI
 ============Quantumultx===============
 [task_local]
 #
 */
 
 const axios = require('axios');
-const CryptoJS = require('crypto-js');
-const qs = require("qs");
 const dayjs = require("dayjs");
+const notify = require('./notify')
 
 function queryLastPrise () {
   return axios.get(`https://service.ierc20.com/api/v1/price/eth`).then(res => {
@@ -26,6 +25,7 @@ function queryLastPrise () {
     return null;
   });
 }
+
 function queryList (price) {
   console.log('单价', price)
   let json = {
@@ -43,46 +43,25 @@ function queryList (price) {
           ...x,
           unitPrice
         }
-      }).filter(x => {
+      });
+      let filterList = list.filter(x => {
         return x.unitPrice < 0.45
       });
-      if (list.length > 0) {
+      if (filterList.length > 0) {
         let now = dayjs().format('HH:mm:ss');
-        let text = list.map(x => {
+        let text = filterList.map(x => {
           let time = dayjs(x.created_at).format('HH:mm:ss')
           return `[${time}] ${x.unitPrice}，${x.amt}`
         }).join('\n');
         console.log('满足条件', text);
-        notify(`43115298698@chatroom`, ``, `温馨提醒 $${price} [${now}] \n${text}`);
+        // prod
+        notify(`43115298698@chatroom`, ``, `温馨提醒(E) $${price} [${now}] \n${text}`);
+        // dev
+        // notify(`44206697810@chatroom`, ``, `温馨提醒 $${price} [${now}] \n${text}`);
       }
-      console.log(orders);
+      console.log(list);
     }
   });
-}
-
-async function notify (fromUser, fromGroup, text, wxids = '') {
-  let url, body
-  if (fromGroup) {
-    if (fromGroup === fromUser) {
-      url = `http://101.35.25.221:19088/api/?type=2`
-      body = { "wxid": fromUser, "msg": text }
-    } else {
-      url = `http://101.35.25.221:19088/api/?type=3`
-      // notify@all
-      body = { "chatRoomId": fromGroup, "msg": `${text}`, "wxids": 'notify@all' }
-    }
-  } else {
-    url = `http://101.35.25.221:19088/api/?type=2`
-    body = { "wxid": fromUser, "msg": text }
-  }
-  return axios({
-    url: url,
-    method: 'POST',
-    data: body,
-    headers: {
-      'content-type': 'application/json, text/plain, */*',
-    }
-  }).then((res) => { }).catch((err) => { })
 }
 
 function main () {
